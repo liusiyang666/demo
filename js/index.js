e2cccb6675c1dbff75e88c3fc63e0a3654474243
liusiyang666 <liusiyang_666@163.com>
2017-07-20 17:23:44 +0800

    hahahahaha

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$.fn.xiala =  function(){
+			this.stop().animate({height:"180px"},1000)
+		}
+		
+		
+		$(".btn1").click(function(e){
+			$(this).hide()
+			$(".list1").xiala()
+			e.stopPropagation()
+		})
+		$(".btn").click(function(e){
+			$(".btn1").show()
+			$(".list1").stop().animate({height:"0px"},1000)
+			e.stopPropagation()
+			
+		})
+		$(".list1 li").click(function(e){
+			$(".text").val( parseInt($(this).html()) )
+			$(".list1").stop().animate({height:"0px"},1000)
+			$(".btn1").show()
+			e.stopPropagation()
+			
+		})
+		$(document).on("click",function(){
+			$(".list1").stop().animate({height:"0px"},1000)
+		})
